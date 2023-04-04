6cc53695d8995c8d85c6ff07447d2148880092a6
Hamza Ak <akhamza88@gmail.com>
2023-04-04 21:51:23 +0300

    init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # gcp-cf-api
 Google Cloud Functions ile örnek bir NodeJs uygulaması geliştirme
+
+gcloud functions deploy cf-app --runtime nodejs18 --trigger-http --allow-unauthenticated --entry-point=api
--- index.js
+++ index.js
@@ -1,32 +1,36 @@
-const express = require('express');
-const app = express();
+/*
+  api parameters;
+    - resource: users or departments
+    - operation: list or find
+    - id: the id of a resource to find
+*/
 
-const PORT = 8080;
 const USERS = [
-  {
-    id: 1,
-    firstName: 'John',
-    lastName: 'Smith'
-  },
-  {
-    id: 2,
-    firstName: 'Jane',
-    lastName: 'Williams'
-  }
+  { id: 1, firstName: 'Hamza', lastName: 'Ak' },
+  { id: 2, firstName: 'Elon', lastName: 'Musk' },
+  { id: 3, firstName: 'John', lastName: 'Doe' }
+];
+const DEPARTMENTS = [
+  { id: 1, firstName: 'Software Department' },
+  { id: 2, firstName: 'Human Resource Department' },
+  { id: 3, firstName: 'Financial Department' }
 ];
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
-app.get('/users', (req, res, next) => {
-  res.json(USERS);
-});
-
-app.get('/users/:userId', (req, res, next) => {
-  res.json(USERS.find(user => user.id === parseInt(req.params.userId)));
-});
-
-module.exports = {
-  app
+exports.api = (req, res) => {
+  switch(req.query.resource) {
+    case 'users': 
+      switch(req.query.operation) {
+        case 'list': res.status(200).send(USERS); break;
+        case 'find': res.status(200).send(USERS.find(user => user.id === parseInt(req.query.id))); break;
+        default: res.status(404).send(`operation: ${req.query.operation} - invalid operation`);
+      };
+      break;
+    case 'departments':
+      switch(req.query.operation) {
+        case 'list': res.status(200).send(DEPARTMENTS); break;
+        case 'find': res.status(200).send(DEPARTMENTS.find(x => x.id === parseInt(req.query.id))); break;
+        default: res.status(404).send(`operation: ${req.query.operation} - invalid operation`);
+      };
+    default: res.status(404).send(`resource: ${req.query.resource} - invalid resource`);
+  }
 };
